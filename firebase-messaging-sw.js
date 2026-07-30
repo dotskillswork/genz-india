9c32e0670ccc0ef9fbc5b919de85c6d39dd69e2a
dotskillswork <dotskills.work@gmail.com>
2026-07-30 17:30:11 +0530

    streamlined study room into clean real-time chat and image notes sharing hub

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,23 @@
-importScripts("https://www.gstatic.com/firebasejs/12.16.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/12.16.0/firebase-messaging-compat.js");
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyCNLwSjHq6LlX9uUovTRHrz9ZfUwEIUWQM",
+    apiKey: "AIzaSyCNLwSjHq6LlX9uUovTRHrz9ZfUWeIUWQM",
     authDomain: "genz-india.firebaseapp.com",
     projectId: "genz-india",
     storageBucket: "genz-india.firebasestorage.app",
     messagingSenderId: "149959470783",
-    appId: "1:149959470783:web:71b7969ee2c8fc049c200a",
-    measurementId: "G-RBRSDRKSRH"
+    appId: "1:149959470783:web:71b7969ee2c8fc049c200a"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: '/favicon.ico'
+        icon: 'https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/72x72/26a1.png'
     };
     self.registration.showNotification(notificationTitle, notificationOptions);
 });
